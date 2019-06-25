b22630e8836f6641dee78429c26ecccf685f4818
Matheus Coelho <matheus.gurgel6@gmail.com>
2019-06-25 18:10:55 -0300

    Formatação da interpretação dos args e correção de bugs
    Formatadas as exceções corretamente, eliminando bugs e tratando bugs de exportação no csv

--- ft.cpp
+++ ft.cpp
@@ -10,9 +10,11 @@ int main(int argc, char** argv) {
     string classe, vet, trat;
     string n;
     string ca;
-    n = argv[argc];
+    int tester;
+
     for (int i = 1; i < argc; i++){
-        ca = argv[i];    
+        ca = argv[i];
+        n = argv[i];    
         if(ca == "-c"){
             classe = argv[i+1];
         }
@@ -24,9 +26,6 @@ int main(int argc, char** argv) {
             trat = argv[i+1];
         }
     }
-
-   
-
     dataBase.open ("database.csv");// Abertura dos arquivos de base
     ArqExt.open(n + ".csv", ios::app); // Abertura do arquivo de exportação
 
@@ -50,8 +49,7 @@ int main(int argc, char** argv) {
                     getline(dataBase,aux,'\n');
                     if (aux == trat){// Comparando e copiando, case true, o 3 elemento da linha
                         expo_trat = aux;
-                        ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n'; 
-                        break;
+                        ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n';     
                     }
                 }
                 
@@ -61,48 +59,48 @@ int main(int argc, char** argv) {
 
 
         //segundo caso, somente os dois primeiros inseridos
-        else if (aux == classe){
-            expo_classe = aux;
-            getline(dataBase,aux,',');
-            if (aux == vet){
-                expo_vet = aux;
-                getline(dataBase,expo_trat,'\n');
-                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n'; 
-                break;
+        if(classe!= "" && vet != "" && trat = ""){
+            if (aux == classe){
+                expo_classe = aux;
+                getline(dataBase,aux,',');
+                if (aux == vet){
+                    expo_vet = aux;
+                    getline(dataBase,expo_trat,'\n');
+                    ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n'; 
+                }
             }
-            
         }
         //fim do segundo caso
 
 
         //terceiro caso, somento o primeiro atributo
-        else if (aux == classe){
-            expo_classe = aux;
-            getline(dataBase,expo_vet,',');
-            getline(dataBase,expo_trat,'\n');
-            ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat <<'\n'; 
-            break;
+        if(classe != "" && vet = "" && trat = ""){
+            if (aux == classe){
+                expo_classe = aux;
+                getline(dataBase,expo_vet,',');
+                getline(dataBase,expo_trat,'\n');
+                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat <<'\n'; 
+            }
         }
         //fim do terceiro caso
 
         //comeco do quarto caso, somente o primeiro e o terceito
-        else if (aux == classe){
-            expo_classe = aux;
-            getline(dataBase,aux,',');
-            expo_vet = aux;
-            getline(dataBase,aux,'\n');
-            if(aux == trat){
-                expo_trat = aux;
-                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat <<'\n'; 
-                break;
+        if(classe != "" && vet = "" && trat != ""){
+            if (aux == classe){
+                expo_classe = aux;
+                getline(dataBase,aux,',');
+                expo_vet = aux;
+                getline(dataBase,aux,'\n');
+                if(aux == trat){
+                    expo_trat = aux;
+                    ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat <<'\n';      
+                }
             }
         }
-        
         //fim do quarto caso
 
         //comeco do quinto caso, somente o segundo e o terceiro atributo
-        
-        else if (true){ // acrescentar que este caso so pode ocorrer quando escolhido 2 (usar contador?) por enquanto esta sendo reutilizado para o 5 caso
+        if(classe = "" && vet != "" && trat != ""){
             expo_classe = aux;
             getline(dataBase,aux,',');
             if(aux == vet){
@@ -111,39 +109,37 @@ int main(int argc, char** argv) {
                 if (aux == trat){
                     expo_trat = aux;
                     ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n';
-                    break;
                 }
-                
             }
         }
         //fim do quinto caso
 
-        //comeco do sexto, somente o segundo atributo ((reutilizando o getline do 4 caso))
-        else if(aux == vet){
-            expo_vet = aux;
-            getline(dataBase,aux,'\n');
-            expo_trat = aux;
-            ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat<<'\n';
-            break;
+        //comeco do sexto, somente o segundo atributo
+        if(classe = "" && vet != "" && trat = ""){
+            expo_classe = aux;
+            getline(dataBase,aux,',');
+            if(aux == vet){
+                expo_vet = aux;
+                getline(dataBase,aux,'\n');
+                expo_trat = aux;
+                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat<<'\n';   
+            }
         }
         // fim do quinto caso
 
         //comeco do setimo caso, somente o terceiro atributo
-        else if(true){
+        if(classe = "" && vet = "" && trat != ""){
+            expo_classe = aux;
+            getline(dataBase,aux,',');
             expo_vet = aux;
             getline(dataBase,aux,'\n');
             if(aux == trat){
                 expo_trat = aux;
                 ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n';
-                break;
             }
 
         }
-        // fim do sexto caso
-        else{
-            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
-            break;
-        }
+        // fim do setimo caso
     }
     
     return 0;
