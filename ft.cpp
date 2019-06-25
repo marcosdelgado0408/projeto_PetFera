1edbfe2f63e3fd4883b2df6a065f83c10f57b2ca
Matheus Coelho <matheus.gurgel6@gmail.com>
2019-06-25 03:51:28 -0300

    Usuário agora escolhe nome do arquivo
    Usuário agora escolhe o nome do arquivo.
    
    Vale ressaltar que até agora o programa realizando suas atividades via entrada de dados após execução. Futuramente estarei implementando novas funcionalidades que permitam que o usuário insira seus dados via argumentos, já na execução do arquivo dentro do terminal.

--- ft.cpp
+++ ft.cpp
@@ -7,15 +7,18 @@ int main(int argc, char** argv) {
     ofstream ArqExt; //usuario escolhe o nome do arquivo
     ifstream dataBase;
     string classe, vet, trat;
+    string n;
+    
     cin >> classe;
     cin >> vet;
     cin >> trat;
+    cin >> n;
     
     
     
 
     dataBase.open ("database.csv");// Abertura dos arquivos de base
-    ArqExt.open("teste.csv", ios::app); // Abertura do arquivo de exportação
+    ArqExt.open(n + ".csv", ios::app); // Abertura do arquivo de exportação
 
 
     while (dataBase.good()){
@@ -36,46 +39,53 @@ int main(int argc, char** argv) {
                 getline(dataBase,aux,'\n');
                 if (aux == trat){// Comparando e copiando, case true, o 3 elemento da linha
                     expo_trat = aux;
-                    ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << ',' << '\n'; 
+                    ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n'; 
                 }
             }
                
-        }else{
-            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
         }
-        //fim do primeiro caso
 
 
         //segundo caso, somente os dois primeiros inseridos
-        if (aux == classe){
+        else if (aux == classe){
             expo_classe = aux;
             getline(dataBase,aux,',');
             if (aux == vet){
                 expo_vet = aux;
                 getline(dataBase,expo_trat,'\n');
-                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << ',' << '\n'; 
+                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n'; 
             }
             
-        }else{
-            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
         }
         //fim do segundo caso
 
 
         //terceiro caso, somento o primeiro atributo
-        if (aux == classe){
+        else if (aux == classe){
             expo_classe = aux;
             getline(dataBase,expo_vet,',');
             getline(dataBase,expo_trat,'\n');
-            ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << ',' << '\n'; 
-        }else{
-            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
+            ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat <<'\n'; 
         }
         //fim do terceiro caso
 
-        //comeco do quarto caso, somente o segundo e o terceiro atributo
+        //comeco do quarto caso, somente o primeiro e o terceito
+        else if (aux == classe){
+            expo_classe = aux;
+            getline(dataBase,aux,',');
+            expo_vet = aux;
+            getline(dataBase,aux,'\n');
+            if(aux == trat){
+                expo_trat = aux;
+                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat <<'\n'; 
+            }
+        }
         
-        if (true){ // acrescentar que este caso so pode ocorrer quando escolhido 2 (usar contador?) por enquanto esta sendo reutilizado para o 5 caso
+        //fim do quarto caso
+
+        //comeco do quinto caso, somente o segundo e o terceiro atributo
+        
+        else if (true){ // acrescentar que este caso so pode ocorrer quando escolhido 2 (usar contador?) por enquanto esta sendo reutilizado para o 5 caso
             expo_classe = aux;
             getline(dataBase,aux,',');
             if(aux == vet){
@@ -83,41 +93,36 @@ int main(int argc, char** argv) {
                 getline(dataBase,aux,'\n');
                 if (aux == trat){
                     expo_trat = aux;
-                    ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << ',' << '\n';
+                    ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n';
                 }
                 
             }
-        }else{
-            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
         }
-        //fim do quarto caso
+        //fim do quinto caso
 
-        //comeco do quinto caso, somente o segundo atributo ((reutilizando o getline do 4 caso))
-        if(aux == vet){
+        //comeco do sexto, somente o segundo atributo ((reutilizando o getline do 4 caso))
+        else if(aux == vet){
             expo_vet = aux;
             getline(dataBase,aux,'\n');
             expo_trat = aux;
-            ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << ',' << '\n';
-        }else{
-            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
+            ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat<<'\n';
         }
         // fim do quinto caso
 
-        //comeco do sexto caso, somente o terceiro atributo
-        if(true){
+        //comeco do setimo caso, somente o terceiro atributo
+        else if(true){
             expo_vet = aux;
             getline(dataBase,aux,'\n');
             if(aux == trat){
                 expo_trat = aux;
-                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << ',' << '\n';
+                ArqExt << expo_classe << ',' << expo_vet << ',' << expo_trat << '\n';
             }
 
-        }else{
-            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
         }
         // fim do sexto caso
-        
-        
+        else{
+            cout << "Não foram encontrados dados no banco de dados com os registros inseridos" << endl;
+        }
     }
     
     return 0;
